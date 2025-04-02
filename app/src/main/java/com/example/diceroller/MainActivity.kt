abb4872cd5cbbfee45175cd7a3f5b0a1625b4cdf
Vishal M. <mish.vish.fl@gmail.com>
2025-04-02 05:33:11 +0530

    updated logic

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,6 +14,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -38,22 +42,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    Column (
+    var result by remember { mutableStateOf(1) }
+    // Determine the image resource based on the dice result.
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(R.drawable.dice_1),
-                    contentDescription = "1"
+            contentDescription = result.toString()
         )
-        // Add vertical spacing between the Image and the Button
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
-
         }
     }
 }
